01188290a3805d0b08decc42dd6645078e3821da
tinyvogon <hoanhk@gmail.com>
2021-05-04 22:38:34 -0700

    remove hidden files

--- Scripts/stopvoila.sh
+++ Scripts/stopvoila.sh
@@ -3,6 +3,8 @@
 if [ -d /home/ubuntu/voila/notebooks ]
 then
 	rm -rf /home/ubuntu/voila/notebooks/*
+	rm  -rf /home/ubuntu/voila/notebooks/.ipynb_checkpoints
+	rm  -rf /home/ubuntu/voila/notebooks/.git
 	sudo systemctl stop voila
 else
 	exit
